6281364448a4f70b799be85883b38f72338eba2b
anjali2410107 <anjaliagarwalnambardar@gmail.com>
2026-03-25 11:02:59 +0530

    added bloc Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'screens/splash_screen.dart';
 import 'theme/app_theme.dart';
+import 'bloc/navigation/navigation_bloc.dart';
+import 'bloc/profile/profile_bloc.dart';
+import 'bloc/chat/chat_bloc.dart';
+import 'bloc/workout_plan/workout_plan_bloc.dart';
+import 'bloc/progress/progress_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,11 +23,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'AI Workout Coach',
-      theme: AppTheme.dark(),
-      home: const SplashScreen(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => NavigationBloc()),
+        BlocProvider(create: (_) => ProfileBloc()..add(ProfileLoaded())),
+        BlocProvider(create: (_) => ChatBloc()..add(ChatInitialized())),
+        BlocProvider(create: (_) => WorkoutPlanBloc()),
+        BlocProvider(create: (_) => ProgressBloc()..add(ProgressLoaded())),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'AI Workout Coach',
+        theme: AppTheme.dark(),
+        home: const SplashScreen(),
+      ),
     );
   }
 }
